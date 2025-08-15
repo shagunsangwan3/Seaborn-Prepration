a2f90f237bc37be5eda94c2ba5809117ef4484fd
shagun sangwan <shagunsangwan738@gmail.com>
2025-08-15 21:50:17 +0530

    Added new files

--- histogramplot.py
+++ histogramplot.py
@@ -8,3 +8,15 @@ print(df)
 # 1
 sns.displot(df["bill_length_mm"])
 plt.show()
+
+# 2
+sns.displot(df["flipper_length_mm"],bins=[170,180,190,200,210,220,230,240])
+plt.show()
+
+# 3 
+sns.displot(df["bill_length_mm"],kde=True)
+plt.show()
+
+# 4
+sns.displot(df["bill_length_mm"],kde=True,color="r",log_scale=True)
+plt.show()
